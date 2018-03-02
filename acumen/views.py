a0bdd7faea4e4a3fbe3899e589b86e6e47488034
Ian Bell <en.bell16@gmail.com>
2018-03-02 16:38:46 -0700

    updates to views, passing context variables into templates

--- acumen/views.py
+++ acumen/views.py
@@ -7,4 +7,24 @@ def index(request):
 	return HttpResponse('This is the Acumen home page')
 
 def ian(request):
-	return HttpResponse("This is ian's page. <a href='/'>return to the home page</a>")
+	#essentially the "route tokens"
+	context = {
+	'middle_name':'james',
+	'last_name':'bell',
+	'rooms':rooms
+	}
+	return render(request,'ian.html',context)
+
+class Room:
+	def __init__(self,owner,level,occupants):
+		self.owner = owner
+		self.level = level
+		self.occupants = occupants
+
+rooms = [
+	Room('Master','Level 1', 2),
+	Room('Jackson','Level 1', 1),
+	Room('Lincoln','Basement',1),
+	Room('Ayla','Baseent',1),
+]
+	
